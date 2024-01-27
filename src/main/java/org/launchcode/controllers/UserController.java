0713ee023e45b5a151526a66665b3ae92ff08c51
Kevin Wiegman <wiegmank@gmail.com>
2024-01-26 18:22:02 -0600

    spa day studio part 2, complete with some bonus

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,25 +12,19 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+
         return "user/add";
     }
 
+
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors) {
+        if(errors.hasErrors() || !user.getPassword().equals(user.getVerifyPassword())) {
             return "user/add";
         }
-
+        return "user/index";
     }
-
 
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,22 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull (message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
@@ -37,7 +50,25 @@ public class User {
     }
 
     public void setPassword(String password) {
+
         this.password = password;
+        checkPassword();
+    }
+
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (password == null || !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
     }
 }
 
